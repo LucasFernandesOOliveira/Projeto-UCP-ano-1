6d29e4e7947480a5c97f4f42feadc242f80db98d
LucasFernandesOOliveira <lucasfoliveira1234@gmail.com>
2025-04-06 19:12:15 -0300

    Add files via upload

---
+++ Web_Design/js/custom.js
@@ -0,0 +1,27 @@
+
+function openNav() {
+    document.getElementById("myNav").classList.toggle("menu_width");
+    document
+        .querySelector(".custom_menu-btn")
+        .classList.toggle("menu_btn-style");
+
+}
+
+var liOne = $(".slider_section .carousel-indicators li.li_one");
+var litwo = $(".slider_section .carousel-indicators li.li_two")
+var ulAfter = $(".slider_section .carousel-indicators .ol_design")
+
+$('#carouselExampleIndicators').on('slid.bs.carousel', function (event) {
+    if (liOne.hasClass("active")) {
+    
+        ulAfter.css({
+            'left': 'calc(0% + ' + 35 + 'px)'
+        })
+
+    } else if (litwo.hasClass("active")) {
+        
+        ulAfter.css({
+            'left': 'calc(100% - ' + 45 + 'px)'
+        })
+    }
+});
